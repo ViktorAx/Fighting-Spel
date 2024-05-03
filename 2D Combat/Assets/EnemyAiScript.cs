e25e291f1ec466ca83b3db311525f35aa571e8b7
SayedAmir69 <150425279+SayedAmir69@users.noreply.github.com>
2024-05-03 11:30:04 +0200

    jojo reference

--- 2D Combat/Assets/EnemyAiScript.cs
+++ 2D Combat/Assets/EnemyAiScript.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAiScript : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject explosionEffect;
+    public Sprite[] spriteOptions; // Array of sprite options for EnemyBullet
+    public float bulletSpeed = 10f;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isFacingRight = true;
+
     void Start()
     {
-        
+        // Set sprite renderer component
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Choose a random sprite from the sprite options
+        if (spriteOptions.Length > 0)
+        {
+            int randomIndex = Random.Range(0, spriteOptions.Length);
+            spriteRenderer.sprite = spriteOptions[randomIndex];
+        }
+
+        // Set initial velocity based on facing direction
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (!isFacingRight)
+        {
+            rb.velocity = new Vector2(-bulletSpeed, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(bulletSpeed, 0f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        // Check collision with player
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        // Check collision with ground or pillar
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Pillar"))
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
